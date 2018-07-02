a666df55047ffeda88332c29e7e9095cac0fb423
P7165693 <P7165693@INGTCWS1130.nessingtc.com>
2018-07-02 16:54:06 +0530

    Controller modified

---
+++ SpringBoot1/src/main/java/com/ness/controller/BusinessController.java
@@ -0,0 +1,27 @@
+package com.ness.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ness.bean.StoreItems;
+import com.ness.service.OrderItemService;
+
+@Controller
+public class BusinessController {
+	@Autowired
+	private OrderItemService orderItemService;
+	@RequestMapping(value="/viewOrders.htm",method = RequestMethod.GET)
+	public ModelAndView viewOrderedItems() {
+		ModelAndView mav=new ModelAndView();
+		List<StoreItems> storeItemsList=orderItemService.fetchOrderItems();
+		mav.addObject("orderlist", storeItemsList);
+		mav.addObject("message", "Welcome to Store!");
+		return mav;
+	}
+}
